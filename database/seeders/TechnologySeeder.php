bee2db7beadccd6d2bc42f1b5686a566e993df8b
NicholasConti <conti94@live.it>
2023-05-23 15:26:40 +0200

    technologues seeder and migration

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +17,18 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['HTML', 'CSS', 'JavaScript', 'VueJS', 'PHP', 'Laravel'];
+
+        Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        foreach ($technologies as $tec) {
+            $newTec = new Technology();
+            $newTec->name = $tec;
+            $newTec->slug = Str::slug($newTec->name);
+
+            $newTec->save();
+        }
     }
 }
